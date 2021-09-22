7fec5eea96ce7537ad346384610c57b49ed3a897
Betty Da <bda@sentry.io>
2021-09-21 20:35:29 -0400

    feat(symbolicator): Add the ability to manipulate the automatic killswitch for the LPQ.

--- tests/sentry/processing/realtime_metrics/test_realtime_metrics_store.py
+++ tests/sentry/processing/realtime_metrics/test_realtime_metrics_store.py
@@ -1,89 +1,79 @@
 import datetime
 import time
-from typing import TYPE_CHECKING, Any, Dict
+from typing import TYPE_CHECKING
 
 import pytest
 
-from sentry.processing import realtime_metrics  # type: ignore
-from sentry.processing.realtime_metrics.redis import RedisRealtimeMetricsStore  # type: ignore
+from sentry.processing.realtime_metrics import base  # type: ignore
 from sentry.utils import redis
 
 if TYPE_CHECKING:
-    from typing import Callable
+    from typing import Callable, TypeVar
 
-    def _fixture(func: Callable[..., Any]) -> Callable[..., None]:
+    # Declare fixture decorator to swallow the function, this isn't the actual type returned
+    # but the type is unusable as a function which is what matters.
+    F = TypeVar("F", bound=Callable[..., None])
+
+    def _fixture(func: F) -> F:
         ...
 
     pytest.fixture = _fixture
 
 
 @pytest.fixture
-def config() -> Dict[str, Any]:
-    return {
-        "cluster": "default",
-        "counter_bucket_size": 10,
-        "counter_ttl": datetime.timedelta(milliseconds=400),
-    }
+def redis_cluster() -> redis._RedisCluster:
+    return redis.redis_clusters.get("default")
 
 
 @pytest.fixture
-def redis_cluster(config: Dict[str, Any]) -> redis._RedisCluster:
-    cluster, options = redis.get_cluster_from_options(
-        "TEST_CLUSTER", config, cluster_manager=redis.redis_clusters
+def store(redis_cluster: redis._RedisCluster) -> base.RealtimeMetricsStore:
+    return base.RealtimeMetricsStore(
+        redis_cluster, counter_bucket_size=10, counter_ttl=datetime.timedelta(milliseconds=400)
     )
-    return cluster
-
-
-@pytest.fixture
-def store(config: Dict[str, Any]) -> RedisRealtimeMetricsStore:
-    return RedisRealtimeMetricsStore(**config)
-
-
-def test_default() -> None:
-    realtime_metrics.increment_project_event_counter(17, 1234)
 
 
 def test_increment_project_event_counter(
-    store: RedisRealtimeMetricsStore, redis_cluster: redis._RedisCluster
+    store: base.RealtimeMetricsStore, redis_cluster: redis._RedisCluster
 ) -> None:
     store.increment_project_event_counter(17, 1147)
-    assert redis_cluster.get("symbolicate_event_low_priority:counter:10:17:1140") == "1"
+    counter = redis_cluster.get("symbolicate_event_low_priority:17:1140")
+    assert counter == "1"
     time.sleep(0.5)
-    assert redis_cluster.get("symbolicate_event_low_priority:counter:10:17:1140") is None
+    counter = redis_cluster.get("symbolicate_event_low_priority:17:1140")
+    assert counter is None
 
 
 def test_increment_project_event_counter_twice(
-    store: RedisRealtimeMetricsStore, redis_cluster: redis._RedisCluster
+    store: base.RealtimeMetricsStore, redis_cluster: redis._RedisCluster
 ) -> None:
     store.increment_project_event_counter(17, 1147)
     time.sleep(0.2)
     store.increment_project_event_counter(17, 1149)
-    assert redis_cluster.get("symbolicate_event_low_priority:counter:10:17:1140") == "2"
+    counter = redis_cluster.get("symbolicate_event_low_priority:17:1140")
+    assert counter == "2"
     time.sleep(0.3)
-    # the second insert should have refreshed the ttl
-    assert redis_cluster.get("symbolicate_event_low_priority:counter:10:17:1140") == "2"
-    time.sleep(0.2)
     # it should have expired by now
-    assert redis_cluster.get("symbolicate_event_low_priority:counter:10:17:1140") is None
+    counter = redis_cluster.get("symbolicate_event_low_priority:17:1140")
+    assert counter is None
 
 
 def test_increment_project_event_counter_multiple(
-    store: RedisRealtimeMetricsStore, redis_cluster: redis._RedisCluster
+    store: base.RealtimeMetricsStore, redis_cluster: redis._RedisCluster
 ) -> None:
     store.increment_project_event_counter(17, 1147)
     store.increment_project_event_counter(17, 1152)
 
-    assert redis_cluster.get("symbolicate_event_low_priority:counter:10:17:1140") == "1"
-    assert redis_cluster.get("symbolicate_event_low_priority:counter:10:17:1150") == "1"
+    assert redis_cluster.get("symbolicate_event_low_priority:17:1140") == "1"
+    assert redis_cluster.get("symbolicate_event_low_priority:17:1150") == "1"
 
 
-def test_get_lpq_projects_unset(store: RedisRealtimeMetricsStore) -> None:
+def test_get_lpq_projects_unset(store: base.RealtimeMetricsStore) -> None:
     in_lpq = store.get_lpq_projects()
     assert in_lpq == set()
 
 
 def test_get_lpq_projects_empty(
-    store: RedisRealtimeMetricsStore, redis_cluster: redis._RedisCluster
+    store: base.RealtimeMetricsStore, redis_cluster: redis._RedisCluster
 ) -> None:
     redis_cluster.sadd("store.symbolicate-event-lpq-selected", 1)
     redis_cluster.srem("store.symbolicate-event-lpq-selected", 1)
@@ -93,7 +83,7 @@ def test_get_lpq_projects_empty(
 
 
 def test_get_lpq_projects_filled(
-    store: RedisRealtimeMetricsStore, redis_cluster: redis._RedisCluster
+    store: base.RealtimeMetricsStore, redis_cluster: redis._RedisCluster
 ) -> None:
     redis_cluster.sadd("store.symbolicate-event-lpq-selected", 1)
     in_lpq = store.get_lpq_projects()
@@ -101,7 +91,7 @@ def test_get_lpq_projects_filled(
 
 
 def test_add_project_to_lpq_unset(
-    store: RedisRealtimeMetricsStore, redis_cluster: redis._RedisCluster
+    store: base.RealtimeMetricsStore, redis_cluster: redis._RedisCluster
 ) -> None:
     assert store.add_project_to_lpq(1)
     in_lpq = redis_cluster.smembers("store.symbolicate-event-lpq-selected")
@@ -109,7 +99,7 @@ def test_add_project_to_lpq_unset(
 
 
 def test_add_project_to_lpq_empty(
-    store: RedisRealtimeMetricsStore, redis_cluster: redis._RedisCluster
+    store: base.RealtimeMetricsStore, redis_cluster: redis._RedisCluster
 ) -> None:
     redis_cluster.sadd("store.symbolicate-event-lpq-selected", 1)
     redis_cluster.srem("store.symbolicate-event-lpq-selected", 1)
@@ -120,7 +110,7 @@ def test_add_project_to_lpq_empty(
 
 
 def test_add_project_to_lpq_dupe(
-    store: RedisRealtimeMetricsStore, redis_cluster: redis._RedisCluster
+    store: base.RealtimeMetricsStore, redis_cluster: redis._RedisCluster
 ) -> None:
     redis_cluster.sadd("store.symbolicate-event-lpq-selected", 1)
 
@@ -130,7 +120,7 @@ def test_add_project_to_lpq_dupe(
 
 
 def test_add_project_to_lpq_filled(
-    store: RedisRealtimeMetricsStore, redis_cluster: redis._RedisCluster
+    store: base.RealtimeMetricsStore, redis_cluster: redis._RedisCluster
 ) -> None:
     redis_cluster.sadd("store.symbolicate-event-lpq-selected", 11)
 
@@ -140,7 +130,7 @@ def test_add_project_to_lpq_filled(
 
 
 def test_remove_projects_from_lpq_unset(
-    store: RedisRealtimeMetricsStore, redis_cluster: redis._RedisCluster
+    store: base.RealtimeMetricsStore, redis_cluster: redis._RedisCluster
 ) -> None:
     removed = store.remove_projects_from_lpq({1})
     assert removed == set()
@@ -150,7 +140,7 @@ def test_remove_projects_from_lpq_unset(
 
 
 def test_remove_projects_from_lpq_empty(
-    store: RedisRealtimeMetricsStore, redis_cluster: redis._RedisCluster
+    store: base.RealtimeMetricsStore, redis_cluster: redis._RedisCluster
 ) -> None:
     redis_cluster.sadd("store.symbolicate-event-lpq-selected", 1)
     redis_cluster.srem("store.symbolicate-event-lpq-selected", 1)
@@ -163,7 +153,7 @@ def test_remove_projects_from_lpq_empty(
 
 
 def test_remove_projects_from_lpq_only_member(
-    store: RedisRealtimeMetricsStore, redis_cluster: redis._RedisCluster
+    store: base.RealtimeMetricsStore, redis_cluster: redis._RedisCluster
 ) -> None:
     redis_cluster.sadd("store.symbolicate-event-lpq-selected", 1)
 
@@ -175,7 +165,7 @@ def test_remove_projects_from_lpq_only_member(
 
 
 def test_remove_projects_from_lpq_nonmember(
-    store: RedisRealtimeMetricsStore, redis_cluster: redis._RedisCluster
+    store: base.RealtimeMetricsStore, redis_cluster: redis._RedisCluster
 ) -> None:
     redis_cluster.sadd("store.symbolicate-event-lpq-selected", 11)
 
@@ -187,7 +177,7 @@ def test_remove_projects_from_lpq_nonmember(
 
 
 def test_remove_projects_from_lpq_subset(
-    store: RedisRealtimeMetricsStore, redis_cluster: redis._RedisCluster
+    store: base.RealtimeMetricsStore, redis_cluster: redis._RedisCluster
 ) -> None:
     redis_cluster.sadd("store.symbolicate-event-lpq-selected", 1)
     redis_cluster.sadd("store.symbolicate-event-lpq-selected", 11)
@@ -200,7 +190,7 @@ def test_remove_projects_from_lpq_subset(
 
 
 def test_remove_projects_from_lpq_all_members(
-    store: RedisRealtimeMetricsStore, redis_cluster: redis._RedisCluster
+    store: base.RealtimeMetricsStore, redis_cluster: redis._RedisCluster
 ) -> None:
     redis_cluster.sadd("store.symbolicate-event-lpq-selected", 1)
     redis_cluster.sadd("store.symbolicate-event-lpq-selected", 11)
@@ -213,7 +203,7 @@ def test_remove_projects_from_lpq_all_members(
 
 
 def test_remove_projects_from_lpq_no_members(
-    store: RedisRealtimeMetricsStore, redis_cluster: redis._RedisCluster
+    store: base.RealtimeMetricsStore, redis_cluster: redis._RedisCluster
 ) -> None:
     redis_cluster.sadd("store.symbolicate-event-lpq-selected", 1)
 
